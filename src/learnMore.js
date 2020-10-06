8d7292c378ca5d5d5c04c206eb6b1cdda97e631e
faizah1992 <faizah1992@gmail.com>
2020-10-06 10:38:09 -0500

    learnmore file added

--- src/learnMore.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-function learnMore() {
-  let teamTitle = document.createElement("h4");
-  teamTitle.innerHTML = "What Makes Us Different";
-  let text = document.createElement("p");
-  text.innerText =
-    "We’re a business that focuses on helping homeowners like you find solutions for your problem whether you’re going through a foreclosure, can’t sell your property, or just need to sell their home for all kinds of reasons. Our vow is to treat you with respect, empathize with your situation and offer the best solution we can for getting your home sold. Yes, we’re not licensed but our intent is to purchase your property As-Is, not list your home like traditional real estate professionals offer. You will be able to utilize our knowledge and expertise free of charge for handling the entire transaction from start to finish without being charged a dime in commissions or fees.";
-}
